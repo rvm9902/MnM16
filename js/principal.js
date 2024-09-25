114317b2074a5909f32b666b599f8aecbba58535
rvm9902 <146854064+rvm9902@users.noreply.github.com>
2024-09-25 09:20:06 -0300

    PRONTO

--- js/principal.js
+++ js/principal.js
@@ -3,8 +3,8 @@ titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll('.paciente');
 for (let contador = 0; contador < pacientes.length; contador++) {
-    var peso = pacientes [contador].querySelector ('.info-peso').textContent
-    var altura = pacientes [contador].querySelector ('.info-altura').textContent
+    var peso = pacientes[contador].querySelector('.info-peso').textContent;
+    var altura = pacientes[contador].querySelector('.info-altura').textContent;
     var imc = peso / (altura * altura);
-    pacientes [contador].querySelector('.info-imc').textContent = imc;
+    pacientes[contador].querySelector('.info-imc').textContent = imc;
 }
